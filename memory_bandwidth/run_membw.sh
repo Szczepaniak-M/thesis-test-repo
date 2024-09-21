5b63f7630d3e5fc6bb1bfed9f4fd9ae0a5e1f118
Szczepaniak-M <m.szczepaniak99@gmail.com>
2024-09-21 17:23:33 +0200

    Memory bandwidth - fix

--- memory_bandwidth/run_membw.sh
+++ memory_bandwidth/run_membw.sh
@@ -2,9 +2,9 @@
 
 echo "threads,bw,sum" > results.csv
 threads=(1 2 4 6 8 12 16 18 20 22 24 28 32 36 40 44 48) 
-# fill 75% of RAM size with uint64_t
-size_kb=$(free -b | grep Mem: | awk '{print $2}')
-input_size=$(echo "$size_kb * 0.75 / 8" | bc)
+# fill 50% of RAM size with uint64_t
+size=$(free -b | grep Mem: | awk '{print $2}')
+input_size=$(echo "$size * 0.5 / 8" | bc)
 
 for t_count in "${threads[@]}";
 do
