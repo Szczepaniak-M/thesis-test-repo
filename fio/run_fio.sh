7ca91c733ea77e3f077563db678e7883cf8ea881
Szczepaniak-M <m.szczepaniak99@gmail.com>
2024-09-22 12:24:01 +0200

    Improve FIO

--- fio/run_fio.sh
+++ fio/run_fio.sh
@@ -3,6 +3,6 @@
 experiments=("write" "randwrite" "read" "randread" "randrw")
 for exp in "${experiments[@]}"
 do
-    output_file="results-${exp}-node.json"
-    sudo fio --name=noisy --rw="${exp}" --numjobs=4 --refill_buffers --ioengine=libaio --time_based --runtime=60s --group_reporting --iodepth=128 --bs=4k --filename=/dev/nvme0n1 --size=20G --direct=1 --output-format=json,terse --output="$output_file"
+    output_file="results-${exp}.json"
+    sudo fio --name=noisy --rw="${exp}" --numjobs=4 --refill_buffers --ioengine=libaio --time_based --runtime=60s --group_reporting --iodepth=128 --bs=4k --filename=/dev/nvme0n1 --size=10G --direct=1 --output-format=json,terse --output="$output_file"
 done
