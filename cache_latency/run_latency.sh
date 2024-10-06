b583a1362e6d85ab5ae102a5664fa1a7d8cbd1a6
Michał Szczepaniak <49671638+Szczepaniak-M@users.noreply.github.com>
2024-10-06 21:09:03 +0200

    Update run_latency.sh

--- cache_latency/run_latency.sh
+++ cache_latency/run_latency.sh
@@ -5,30 +5,25 @@ convert_to_bytes() {
     unit=$2
 
     case $unit in
-        KiB) echo $(($size * 1024)) ;;
-        MiB) echo $(($size * 1024 * 1024)) ;;
-        GiB) echo $(($size * 1024 * 1024 * 1024)) ;;
+        KiB) echo $(echo "$size * 1024" | bc) ;;
+        MiB) echo $(echo "$size * 1024 * 1024" | bc) ;;
+        GiB) echo $(echo "$size * 1024 * 1024 * 1024" | bc) ;;
         *) echo $size ;;
     esac
 }
 
-# Run lscpu and extract L1d, L2, and L3 cache sizes
 lscpu_output=$(lscpu)
 
-# Extract relevant cache values
 l1d=$(echo "$lscpu_output" | grep "L1d" | awk '{print $3, $4}')
 l2=$(echo "$lscpu_output" | grep "L2" | awk '{print $3, $4}')
 l3=$(echo "$lscpu_output" | grep "L3" | awk '{print $3, $4}')
 
-# Split size and unit and convert to bytes
 l1d_bytes=$(convert_to_bytes $(echo $l1d))
 l2_bytes=$(convert_to_bytes $(echo $l2))
 l3_bytes=$(convert_to_bytes $(echo $l3))
 
 cache_sizes=("$l1d_bytes" "$l2_bytes" "$l3_bytes")
 threads=(1 2 4 6 8 12 16 18 20 22 24 28 32 36 40 44 48)
-
-echo "${cache_sizes[@]}"
 
 repeat=100
 ool=100
@@ -37,7 +32,6 @@ do
   input_size=($(echo "$cache_size * 0.8 / 8" | bc))
   for t_count in "${threads[@]}";
   do
-    echo "$input_size $t_count"
     ./latency $t_count $input_size $repeat $ool >> results.txt
   done
 done
